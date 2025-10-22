3f1ee35cb149f512f0755b68aa7daa6bf331f2df
haybdev <officewebb01@gmail.com>
2025-10-22 16:11:15 +0100

    new update

--- src/pages/about-jca.jsx
+++ src/pages/about-jca.jsx
@@ -4,6 +4,7 @@ import "../styles/pages/about-jca.css";
 
 // Import media
 import Vector from "../media/images/Vector.png"
+import VectorOrange from "../media/images/VectorOrange.png"
 
 export function AboutJca(){
     return(
@@ -18,6 +19,16 @@ export function AboutJca(){
                   <p>ABOUT RCCG JCA</p>
                 </div>
                 <p>MISSION & VISION</p>
+              </div>
+            </section>
+            <section className="church-vision">
+              <div className="church-vision-box">
+                <div className="icon-with-name">
+                  <img src={VectorOrange} alt="icon" />
+                  <p>RCCG, JESUS CHRIST ASSEMBLY, BATH.</p>
+                </div>
+                <h2>Church Vision</h2>
+                <p>Discipleship and Capacity Building For Global Impact. <br /> <span>Nehemiah 2:17-18</span></p>
               </div>
             </section>
           </main>
