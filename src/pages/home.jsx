1b4d57a865eb36a7a791c7fceaa4a22291f8e83d
juba <juba.jeremiah@gmail.com>
2025-10-24 00:39:05 +0100

    New section in home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,6 +21,7 @@ import pastorandwife from "../media/images/pastorandwife.png"
 import pstmrs from "../media/images/pstmrs.png"
 import congregation from "../media/images/congregation.png"
 import yellowicon from "../media/images/yellow-icon.png"
+import praying from "../media/images/praying.png"
 
 export function Home(){
   
@@ -144,6 +145,59 @@ export function Home(){
                 </p>
               </div>
             </div>
+          </div>
+
+          {/* Fourth Container */}
+          <div className="fourth-continer">
+            <div className="f-c-inset">
+              <div>
+                <div className="tittle-container">
+                  <div className="f-c-tag">
+                    <img src={yellowicon} alt="Yellow icon" />
+                    <p>SERVICES</p>
+                  </div>
+                  <div className="f-c-tittle">
+                    <h3>Our Sunday</h3>
+                    <h2>SERVICE</h2>
+                  </div>
+                </div>
+
+                <div className="inner-boxes">
+                  <div className="box-one">
+                    <h4>SUNDAY SERVICES</h4>
+                    <p>2:00 PM - 4:00 PM</p>
+                  </div> 
+                  <div className="box-two-three">
+                      <div className="box-two">
+                        <div className="b-t-contents">
+                          <h4><span>1ST</span> SUNDAY <br /> OF THE MONTH</h4>
+                        <p>2:00 PM - <br /> 4:30 PM</p>
+                        </div>
+                      </div>
+                      
+                      <div className="box-three">
+                        <div className="b-t-content">
+                          <h4><span>OTHER</span> SUNDAYS <br /> OF THE MONTH</h4>
+                          <p>2:00 PM - <br /> 4:00 PM</p>
+                        </div>
+
+                      </div>
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="f-c-image-lg">
+                <img src={praying} alt="Image of Congregtion praying mode" />
+              </div>
+            </div>
+
+            <Link to={'/contact-us'} className="link">
+                <button>
+                  <p>Learn more</p>
+                  <FaArrowRightLong className="arrow"/>
+                </button>
+            </Link>
           </div>
 
           {/* Ride with us and location section */}
