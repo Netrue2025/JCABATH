204fba6786772374f81e3f7373e9f0674376408b
juba <juba.jeremiah@gmail.com>
2025-10-31 20:08:06 +0000

    JCA good women page

--- src/components/ministriesdropdown.jsx
+++ src/components/ministriesdropdown.jsx
@@ -7,8 +7,8 @@ export function Ministriesdropdown(props){
             <div className="ministrydropdown" id={props.id}>
                 <div className="mini-inner-contents">
                     <Link className="link" to="/amazing-grace"><div className="dropdown-btn"><p>Amazing Grace</p></div></Link>
-                    <Link className="link"><div className="dropdown-btn"><p>AJCA Good Women</p></div></Link>
-                    <Link className="link"><div className="dropdown-btn"><p>Raising Giants</p></div></Link>
+                    <Link className="link" to="/jca-good-women"><div className="dropdown-btn"><p>AJCA Good Women</p></div></Link>
+                    <Link className="link" ><div className="dropdown-btn"><p>Raising Giants</p></div></Link>
                     <Link className="link"><div className="dropdown-btn"><p>Children's Department</p></div></Link>
                 </div>
                
--- src/pages/jca-good-women.jsx
+++ src/pages/jca-good-women.jsx
@@ -1,12 +1,67 @@
-import { Link } from "react-router-dom";
+import { Topnavbar } from "../components/topnavbar"
+import { Header } from "../components/header"
+import vectororange from "../media/images/VectorOrange.png"
+import goodwomenimage from "../media/images/goodwomen.png"
+import { Location } from "../components/location"
+import { Footer } from "../components/footer"
+import "../styles/pages/goodwomen.css"
 
 export function JcaGoodWomen(){
     return(
         <>
-          <h1>This is Jca Good Women page</h1>
-          <Link to={"/"}>
-            <button>👈Back Home</button>
-          </Link>
+            {/* Top navigation bar section */}
+            <Topnavbar />
+
+            {/* Header section */}
+            <Header tag={vectororange} tagname="MINISTRIES" tittle="JCA Good Women"/>
+
+            {/* Main contents section */}
+            <section className="j-g-w-main-section">
+                <div className="g-w-image">
+                    <img src={goodwomenimage} alt="Good women image" />
+                </div>
+
+           
+                <div className="g-w-contents">
+                    <div className="g-w-section-one">
+                        <div className="section-one-head">
+                            <div className="tag-container">
+                                <img src={vectororange} alt="Yellow tage" />
+                                <p>JCA GOOD WOMEN</p>
+                            </div>
+                            <h3>Vision</h3>
+                        </div>
+                        <p>
+                            <ol>
+                                <li>To build women with strong knowledge of God.</li>
+                                <li>Women who are extremely valuable and
+                                    extraordinary like Deborah sharpening the home,
+                                    church and society for Christ.
+                                </li>
+                            </ol> 
+                        </p>
+                    </div>
+
+                    <div className="g-w-section-two">
+                        <h3>JCA Bath Good Women are:</h3>
+                        <p>
+                            <ol>
+                                <li><span>Unique</span> - with strength and courage (Judges 4:1-5, 31)</li>
+                                <li><span>Valuable</span>  - priceless (Proverbs 31:10-31)</li>
+                                <li><span>Honourable</span> - (Esther 4:10, Psalms 45:9)</li>
+                            </ol>
+                        </p>
+                    </div>
+                    <h4>You’re Welcome to join us!</h4>
+                </div>
+            </section>
+
+            {/* Location section  */}
+            <Location />
+
+            {/* Footer Section */}
+            <Footer />
+
           
         </>
     )
