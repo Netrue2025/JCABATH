ccef5a345f74d00dc2582e4a52ea8070e919c0b5
juba <juba.jeremiah@gmail.com>
2025-11-03 12:36:00 +0000

    Little update made

--- src/components/connectdropdown.jsx
+++ src/components/connectdropdown.jsx
@@ -7,7 +7,7 @@ export function Connectdropdown(props){
             <div className="connectdropdown" id={props.id}>
                 <div className="mini-inner-contents">
                     <Link className="link"><div className="dropdown-btn"><p>Services</p></div></Link>
-                    <Link className="link" to="prayer-request"><div className="dropdown-btn"><p>Prayer Request</p></div></Link>
+                    <Link className="link" to="/prayer-request"><div className="dropdown-btn"><p>Prayer Request</p></div></Link>
                     <Link className="link"><div className="dropdown-btn"><p>Contact Us</p></div></Link>
                 </div>
                
