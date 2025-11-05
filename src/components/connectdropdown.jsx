3f0c1cdb2b7208d6b1c91dee70da3dbee5bd3083
haybdev <officewebb01@gmail.com>
2025-11-06 00:04:50 +0100

    update

--- src/components/connectdropdown.jsx
+++ src/components/connectdropdown.jsx
@@ -8,7 +8,7 @@ export function Connectdropdown(props){
                 <div className="mini-inner-contents">
                     <Link className="link"><div className="dropdown-btn"><p>Services</p></div></Link>
                     <Link className="link" to="/prayer-request"><div className="dropdown-btn"><p>Prayer Request</p></div></Link>
-                    <Link className="link"><div className="dropdown-btn"><p>Contact Us</p></div></Link>
+                    <Link className="link" to="/contact-us"><div className="dropdown-btn"><p>Contact Us</p></div></Link>
                 </div>
                
             </div>
--- src/pages/contact-us.jsx
+++ src/pages/contact-us.jsx
@@ -1,13 +1,120 @@
 import { Link } from "react-router-dom";
+import "../styles/pages/contact-us.css";
 
-export function ContactUs(){
-    return(
-        <>
-          <h1>This is the contact Us page</h1>
-          <Link to={"/"}>
-            <button>👈Back Home</button>
-          </Link>
-          
-        </>
-    )
+// Import Components
+import { Topnavbar } from "../components/topnavbar";
+import { Header } from "../components/header";
+import { Location } from "../components/location";
+import { Footer } from "../components/footer";
+
+// Import media
+import VectorOrange from "../media/images/VectorOrange.png";
+import whatsappicon from "../media/images/whatsappicon.png";
+import whatsappiconsvg from "../media/images/whatsappiconsvg.png";
+import instagramicon from "../media/images/instagramicon.png";
+import youtubeicon from "../media/images/youtubeicon.png";
+import facebookicon from "../media/images/facebookicon.png";
+import submitarrowicon from "../media/images/submitarrowicon.png";
+
+export function ContactUs() {
+  return (
+    <>
+      {/* Import Topnavbar component */}
+      <Topnavbar />
+
+      {/* Main section of the webpage */}
+      <main>
+        {/* Import Contact-us screen */}
+        <Header tag={VectorOrange} tagname="CONNECT" tittle="Contact Us" />
+        
+        {/* Contact details section */}
+        <section className="contact-container">
+          <div className="inner-container">
+            <div className="contact-details">
+              <div className="get-in-touch">
+                <h2>Get in touch</h2>
+                <p>
+                  You can fill the form to your right to leave us a message or
+                  contact us through our details for an immediate response.
+                </p>
+                <div className="contact-information">
+                  <div className="contact contact-email">
+                    <p>Email address</p>
+                    <a href="mailto:info@jesuschristassembly.org">
+                      info@jesuschristassembly.org
+                    </a>
+                  </div>
+                  <div className="contact contact-phone">
+                    <p>Phone number</p>
+                    <a href="tel:+447467160789">+44 7467160789</a>
+                  </div>
+                  <div className="contact contact-whatsapp">
+                    <p>
+                      WhatsApp <br />
+                      (Pastor Farouq)
+                    </p>
+                    <a href="#">
+                      <img src={whatsappicon} alt="icon" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="social-media-handles">
+                <p>Follow our Social Media handles</p>
+                <div className="social-media-icons">
+                  <a href="#" className="facebook-link">
+                    <img src={facebookicon} alt="facebook-icon" />
+                  </a>
+                  <a href="#" className="instagram-link">
+                    <img src={instagramicon} alt="instagram-icon" />
+                  </a>
+                  <a href="#" className="youtube-link">
+                    <img src={youtubeicon} alt="youtubeicon-icon" />
+                  </a>
+                  <a href="#" className="whatsapp-link">
+                    <img src={whatsappiconsvg} alt="whatsappiconsvg-icon" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <form className="contact-form">
+              <label htmlFor="">
+                <p>Full Name</p>
+                <input
+                  type="text"
+                  id="input-name"
+                  placeholder="Enter full name"
+                />
+              </label>
+              <label htmlFor="">
+                <p>Email</p>
+                <input type="email" id="" placeholder="Enter email" />
+              </label>
+              <label htmlFor="">
+                <p>Phone number</p>
+                <input type="number" id="" placeholder="Enter phone number" />
+              </label>
+              <label htmlFor="">
+                <p>Message</p>
+                <input
+                  type="text"
+                  id="input-message"
+                  placeholder="Tell us about your request"
+                />
+              </label>
+              <button className="submit-btn" type="submit">
+                Submit <img src={submitarrowicon} alt="icon" className="arrow-icon" />
+              </button>
+            </form>
+          </div>
+        </section>
+
+        {/* Import Location component */}
+        <Location/>
+      </main>
+
+      {/* Import Footer component */}
+      <Footer/>
+    </>
+  );
 }
