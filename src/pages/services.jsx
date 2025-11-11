21ed42a557d44aa2a63fe575cfc6fed74bd35a2d
juba <juba.jeremiah@gmail.com>
2025-11-11 13:02:35 +0000

    Latest commit as of tuesday

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -10,48 +10,7 @@ export function Services(){
         <>
           {/* Top nav bar */}
           <Topnavbar />
-          <div className="fourth-continer">
-            <div className="f-c-inset">
-              <div>
-                <div className="tittle-container">
-                  <div className="f-c-tag">
-                    <img src={yellowicon} alt="Yellow icon" />
-                    <p>SERVICES</p>
-                  </div>
-                  <div className="f-c-tittle">
-                    <h3>Our Sunday</h3>
-                    <h2>SERVICE</h2>
-                  </div>
-                </div>
-
-                <div className="inner-boxes">
-                  <div className="box-one">
-                    <h4>SUNDAY SERVICES</h4>
-                    <p>2:00 PM - 4:00 PM</p>
-                  </div> 
-                  <div className="box-two-three">
-                      <div className="box-two">
-                        <div className="b-t-contents">
-                          <h4><span>1ST</span> SUNDAY <br /> OF THE MONTH</h4>
-                        <p>2:00 PM - <br /> 4:30 PM</p>
-                        </div>
-                      </div>
-                      
-                      <div className="box-three">
-                        <div className="b-t-content">
-                          <h4><span>OTHER</span> SUNDAYS <br /> OF THE MONTH</h4>
-                          <p>2:00 PM - <br /> 4:00 PM</p>
-                        </div>
-
-                      </div>
-                    </div>
-
-                </div>
-              </div>
-                <img src={praying} alt="Image of Congregtion praying mode" />
-              
-            </div>
-          </div>
+   
           
         </>
     )
